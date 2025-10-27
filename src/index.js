a3c371f81d90197e2f135537badaa85b5b6e5c2d
wojtek-smyczek <wojciech.smyczek@student.uj.edu.pl>
2025-10-27 15:56:27 +0100

    Logic in JS for switching the images and dots in the bottom of UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import "./styles.css";
+
+
+const leftArrow = document.querySelector('.left');
+const rightArrow = document.querySelector('.right');
+
+const gallery = document.querySelectorAll('.carousel-image');
+const dots = document.querySelectorAll('.dot');
+
+let counter = 0;
+showGallery(counter);
+
+leftArrow.addEventListener("click", () => {
+    counter--;
+    showGallery();
+})
+
+rightArrow.addEventListener("click", () => {
+
+    counter++;
+    showGallery();
+})
+
+function showGallery() {
+    gallery.forEach((el) => {
+        el.classList.remove('visible');
+
+    })
+
+    dots.forEach(el => {
+        el.classList.remove('dot-selected')
+    })
+    if (counter < 0) {
+        counter = gallery.length - 1;
+    }
+    if (counter >= gallery.length) {
+        counter = 0;
+    }
+    gallery[counter].classList.add('visible');
+    dots[counter].classList.add('dot-selected')
+}
+
+
